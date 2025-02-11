2e9e7b2a899e44fb1dd80ebbaf6fbd78db6d243f
vanirvan <vanir1812@gmail.com>
2025-02-11 22:01:18 +0700

    config: setup react and tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
